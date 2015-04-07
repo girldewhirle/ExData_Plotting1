de6241a705f7e24360604e8223a52c4d7c012ee5
girldewhirle <rebecca.ratcliffe@gmail.com>
2015-04-07 20:05:34 +0100

    plot1 update
    combo_date to datetime for consistency

--- plot1.R
+++ plot1.R
@@ -13,7 +13,7 @@ house_hold_power$Date<-dmy(house_hold_power$Date)
 ## subset 1st and 2nd Feb 2007
 date_subset<-filter(house_hold_power,Date>=dmy("01/02/2007")&Date<=dmy("02/02/2007"))
 ## combine date and time into one field
-date_subset<-mutate(date_subset,combo_date=as.POSIXct(paste(date_subset$Date, date_subset$Time), format="%Y-%m-%d %H:%M:%S"))
+date_subset<-mutate(date_subset,datetime=as.POSIXct(paste(date_subset$Date, date_subset$Time), format="%Y-%m-%d %H:%M:%S"))
 ## convert Global Active Power to numeric via character
 date_subset$Global_active_power<-as.character(date_subset$Global_active_power)
 date_subset$Global_active_power<-as.numeric(date_subset$Global_active_power)
